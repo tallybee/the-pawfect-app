4478797d2ccbf1d96eb0f92360f8a2ad78841621
limadebarros <limadebarros84@gmail.com>
2019-05-14 16:23:50 +0200

    Game one edited

--- src/components/ThreeDogsImage.js
+++ src/components/ThreeDogsImage.js
@@ -1,27 +1,5 @@
 import React from 'react'
 
-<<<<<<< HEAD
-export default function ThreeDogsImage (props) {    
-  const { images } = props 
-
-  console.log(props)
-
-  return (
-      <div className="three-dog-image">
-          <h1>These three dogs</h1>
-
-
-          <br></br>
-
-          <Link to="/">Back</Link>
-          <div>
-          { images && images.map(url => <img key={url} src={ url } alt="Dog" />) }
-          { !images && 'Loading...' }
-          </div>
-      
-      </div>
-  )
-=======
 export default function ThreeDogsImage (props) {  
   const { images } = props 
   
@@ -43,5 +21,4 @@ const style = {
     width: '250px',
     margin: '5px'
   }
->>>>>>> 0a57e0eab71278afbb678566cc4e32a4e2fae6c8
 }
