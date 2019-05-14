40fd348d03f7d027c336f8324b32a1f3bd09c7e9
limadebarros <limadebarros84@gmail.com>
2019-05-14 15:59:36 +0200

    Damn git

--- src/components/ThreeDogsImage.js
+++ src/components/ThreeDogsImage.js
@@ -1,25 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function ThreeDogsImage (props) {
-    
-    const { images } = props 
-    
-    console.log(props)
+export default function ThreeDogsImage (props) {    
+  const { images } = props 
 
-    return (
-        <div className="three-dog-image">
-            <h1>These three dogs</h1>
+  console.log(props)
+
+  return (
+      <div className="three-dog-image">
+          <h1>These three dogs</h1>
 
 
-            <br></br>
+          <br></br>
 
-            <Link to="/">Back</Link>
-            <div>
-            { images && images.map(url => <img key={url} src={ url } alt="Dog" />) }
-            { !images && 'Loading...' }
-            </div>
-        
-        </div>
-    )
+          <Link to="/">Back</Link>
+          <div>
+          { images && images.map(url => <img key={url} src={ url } alt="Dog" />) }
+          { !images && 'Loading...' }
+          </div>
+      
+      </div>
+  )
 }
