4881635bf1598c6dbecbabb89a4f939ac2ae3011
rafaelrolivares <rafaelrolivares@gmail.com>
2019-05-14 15:20:23 +0200

    one dog to be guessed

--- src/components/GameOne.js
+++ src/components/GameOne.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import OneDogImageContainer from './OneDogImageContainer'
 
 export default class GameOne extends Component {
   render() {
@@ -6,6 +7,7 @@ export default class GameOne extends Component {
       <div>
         <h2>Game One</h2>
         <p>TESTING</p>
+        <OneDogImageContainer/>
       </div>
     )
   }
