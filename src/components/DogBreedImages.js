e536754bb9b11c87db3c8bd6fed4053e4797bb11
limadebarros <limadebarros84@gmail.com>
2019-05-14 10:33:31 +0200

    Tabs edited

--- src/components/DogBreedImages.js
+++ src/components/DogBreedImages.js
@@ -4,23 +4,21 @@ import { Link } from 'react-router-dom'
 
 export default function DogBreedImages (props) {
     
-    const { images } = props 
-    
-    console.log(props)
+  const { images } = props 
+  
+  console.log(props)
 
-    return (
-        <div className="dog-breed-images">
-            <h1>Dog Breed Images</h1>
+  return (
+    <div className="dog-breed-images">
+      <h1>Dog Breed Images</h1>
 
+      <br></br>
 
-            <br></br>
-
-            <Link to="/">Go back to the index</Link>
-            <div>
-            { images && images.map(url => <img key={url} src={ url } alt="Dog" />) }
-            { !images && 'Loading...' }
-            </div>
-        
-        </div>
-    )
+      <Link to="/">Go back to the index</Link>
+      <div>
+      { images && images.map(url => <img key={url} src={ url } alt="Dog" />) }
+      { !images && 'Loading...' }
+      </div>    
+    </div>
+  )
 }
--- src/components/DogsList.js
+++ src/components/DogsList.js
@@ -3,23 +3,30 @@ import { Link } from 'react-router-dom'
 
 export default class DogsList extends Component {
 
-    renderDogBreed(breed) {
-        return <li key={breed}>
-            <Link to={ `/dog-breeds/${breed}` }>{breed}</Link>
-        </li>
-    }
+  renderDogBreed(breed) {
+    return <li key={breed}>
+        <Link to={ `/dog-breeds/${breed}` }>{breed}</Link>
+    </li>
+  }
 
-    render() {
-        const { dogBreeds } = this.props
-        return (
-            <div className="dogs-list">
-            <h1>Dogs List</h1>
+  render() {
+    const { dogBreeds } = this.props
+    return (
+        <div className="dogs-list">
+        <h1>Dogs List</h1>
 
-            { !dogBreeds && 'Loading' }
-            { <ul>
-            { dogBreeds && dogBreeds.map(this.renderDogBreed) }
-            </ul> }
-            </div>
-        )
+        { !dogBreeds && 'Loading' }
+        { <ul >
+        { dogBreeds && dogBreeds.map(this.renderDogBreed) }
+        </ul> }
+        </div>
+      )
+  }
+}
+
+
+const style = {
+    uList: {
+        heigth: 
     }
 }
