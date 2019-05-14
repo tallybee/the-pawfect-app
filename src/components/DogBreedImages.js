b6eaf7bfc6db72e0f9d5531187c04c01e2c22aae
limadebarros <limadebarros84@gmail.com>
2019-05-14 15:01:50 +0200

    Some changes to DogBreedImagesContainer and DogList.css

--- src/components/DogBreedImages.js
+++ src/components/DogBreedImages.js
@@ -4,13 +4,28 @@ export default function DogBreedImages(props) {
   const { images } = props
   return (
     <div className='dog-breed-images'>
-      <h1>Dog Breeds Images</h1>
-        Here are 10 pictures of {props.breed}
+      <h1>{props.breed} Images</h1>
+        <p>Her are 10 images</p>
       
-      <div>
-        { images && images.map(url => <img key={ url } src={ url } alt="Dog" />) }
+      <div style={styles.divImg}>
+        { images && images.map(url => <img style={styles.img} key={ url } src={ url } alt="Dog" />) }
         { !images && 'Loading...' }
       </div>
     </div>
   )
+}
+
+const styles = {
+  divImg: {
+    columns: '5',
+    margin: '25px'
+
+  },
+  
+  img: {
+    borderRadius: '10px',
+    border: '1px solid #000',
+    padding: '5px',
+    width: '200px'
+  }
 }
