59bf0cf48ecc027ad1f94d6978a5d0c16253c7db
limadebarros <limadebarros84@gmail.com>
2019-05-15 15:36:21 +0200

    GameOne updated and reduced

--- src/components/GameOne.js
+++ src/components/GameOne.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react'
 import OneDogImageContainer from './OneDogImageContainer'
-import Game from './game1'
 import { GameOneOptionsContainer } from './GameOneOptionsContainer';
+import { connect } from "react-redux";
 
-export default class GameOne extends Component {
+class GameOne extends Component {
+  handleChoice = (guessedBreed) => {
+    if (guessedBreed === this.props.correctDogBreed) {
+      this.props.dispatch({
+        type: "CORRECT_GUESS",
+        payload: guessedBreed
+      });
+    } else {
+      this.props.dispatch({
+        type: "WRONG_GUESS",
+        payload: guessedBreed
+      });
+    };
+  }
+
   render() {
     return (
       <div>
         <OneDogImageContainer/>
         <GameOneOptionsContainer/>
-        <Game />
+        <h3>What am I?</h3>
+        <p onClick={() => this.handleChoice(this.props.correctDogBreed)}>
+        { this.props.correctDogBreed }
+        </p>
+        <p onClick={() => this.handleChoice("not hound")}>
+          {" "}
+          wrong breed{" "}
+        </p>
+        <p onClick={() => this.handleChoice("a cat")}> wrong breed </p>
       </div>
     )
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    correctDogBreed: state.correctDogBreed,
+    selectedDogBreed: state.selectedDogBreed
+  };
+};
+
+export default connect( mapStateToProps)(GameOne);
