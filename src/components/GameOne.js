280f7e3fbda42dfa3a8bc14db55b3d05e5e65866
tallybee <talbondareva@gmail.com>
2019-05-16 17:51:30 +0200

    categories

--- src/components/GameOne.js
+++ src/components/GameOne.js
@@ -1,19 +1,16 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import request from "superagent";
-import DoggoHappy from '../img/doggohappy.png'
-import DoggoSad from '../img/doggosad.png'
-import soundwin from '../sounds/shootingstar.mp3'
-import soundfail from '../sounds/fail.mp3'
-import Sound from 'react-sound'
 import "./GameOne.css";
+const Mousetrap = require("mousetrap");
 
 class GameOne extends Component {
-  state = { 
+
+  state = {
     options: [],
     correctDogBreed: null,
-    images: null 
-  }
+    images: null
+  };
 
   componentDidMount() {
     request
@@ -66,7 +63,7 @@ class GameOne extends Component {
   handleChoice = guessedBreed => {
     if (
       guessedBreed === this.props.correctDogBreed &&
-      this.props.roundsPlayed < 5
+      this.props.roundsPlayed < 10
     ) {
       this.props.dispatch({
         type: "CORRECT_GUESS",
@@ -75,7 +72,7 @@ class GameOne extends Component {
       this.componentDidMount();
     } else if (
       guessedBreed !== this.props.correctDogBreed &&
-      this.props.roundsPlayed < 5
+      this.props.roundsPlayed < 10
     ) {
       this.props.dispatch({
         type: "WRONG_GUESS",
@@ -90,79 +87,68 @@ class GameOne extends Component {
     }
   };
 
-  restarteGame() {
-    window.location.reload()
+  restartGame() {
+    window.location.reload();
   }
 
   render() {
+    Mousetrap.bind("1", () => this.handleChoice(this.state.options[0]));
+    Mousetrap.bind("2", () => this.handleChoice(this.state.options[1]));
+    Mousetrap.bind("3", () => this.handleChoice(this.state.options[2]));
+    Mousetrap.bind("enter", () => this.restartGame());
 
-    if (this.props.roundsPlayed === 5 && this.props.score === 5) {
-      return (
-        <div className='GameWin'>
-          <img src={DoggoHappy} alt="Dog sad"/>
-          <h2>You WIN</h2>
-          <div>
-            <h3>You have {this.props.score} correct guesses.</h3>
-            <Sound
-              url={soundwin}
-              playStatus={Sound.status.PLAYING}
-              playFromPosition={300 /* in milliseconds */}
-              onLoading={this.handleSongLoading}
-              onPlaying={this.handleSongPlaying}
-              onFinishedPlaying={this.handleSongFinishedPlaying}
-            />
-          </div>
-          <div>
-          <h3>You are the most pawfect doggo lover!</h3>
-        </div>
-          <button onClick={this.restarteGame}>Start New Game</button>
-        </div>)
-    } else if(this.props.roundsPlayed === 5 && this.props.score < 5) {
+    if (this.props.roundsPlayed === 10) {
       const categories = [
-        'You are not much into dogs, are you?',
-        "You were lucky once or twice, but you don't really know dogs", 'can still improve', 
-        'you are getting there', 
-        'You are not there yet']
-
+        "You are not much into dogs, are you?",
+        "This made me wanna howl",
+        "Still room for imp-woof-ment!",
+        "Pawsitive result",
+        "You know ya dawgs!",
+        "You are the most pawfect dog lover!"
+      ];
       return (
-        <div className='GameOver'>
-          <img src={DoggoSad} alt="Dog sad"/>
-          <h2>GAME OVER</h2>
+        <div>
+          <h2>Game Over</h2>
+          <img src="" alt="" />
           <div>
             <h3>You have {this.props.score} correct guesses.</h3>
-            <Sound
-              url={soundfail}
-              playStatus={Sound.status.PLAYING}
-              playFromPosition={300 /* in milliseconds */}
-              onLoading={this.handleSongLoading}
-              onPlaying={this.handleSongPlaying}
-              onFinishedPlaying={this.handleSongFinishedPlaying}
-            />
           </div>
           <div>
-          <h3>{categories[this.props.score]}</h3>
+            <h3>{categories[Math.floor(this.props.score/2)]}</h3>
+          </div>
+          <button onClick={this.restartGame}>Start New Game</button>
         </div>
-          <button onClick={this.restarteGame}>Start New Game</button>
-        </div>)
-    }
-
-    return (
-      <div>
-        <h1>What breed am I?</h1>
-         <img style={styles.img} src={this.state.images} alt='dawg'/>
+      );
+    } else {
+      return (
         <div>
-          <h4>Check me out, dawg!</h4>
-        <button onClick={() => this.handleChoice(this.state.options[0])}>
-        { this.state.options[0] }
-        </button>
-        <button onClick={() => this.handleChoice(this.state.options[1])}>
-          {this.state.options[1]}        
-        </button>
-        <button onClick={() => this.handleChoice(this.state.options[2])}> {this.state.options[2]} </button>
-        <h3>You guessed the breed of {this.props.score} out of {this.props.roundsPlayed} dogs.</h3>
+          <h1>What breed am I?</h1>
+          <img style={styles.img} src={this.state.images} alt="dawg" />
+          <div>
+            <h4>Check me out, dawg!</h4>
+            <button onClick={() => this.handleChoice(this.state.options[0])}>
+              {" "}
+              1.
+              {this.state.options[0]}
+            </button>
+            <button onClick={() => this.handleChoice(this.state.options[1])
+            }>
+              {" "}
+              2.
+              {this.state.options[1]}
+            </button>
+            <button onClick={() => this.handleChoice(this.state.options[2])}>
+              {" "}
+              3. {this.state.options[2]}{" "}
+            </button>
+            <h3>
+              You guessed the breed of {this.props.score} out of{" "}
+              {this.props.roundsPlayed} dogs.
+            </h3>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
@@ -176,11 +162,11 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect( mapStateToProps)(GameOne);
+export default connect(mapStateToProps)(GameOne);
 
 const styles = {
   img: {
-    width: '350px',
-    borderRadius: '10px'
+    width: "350px",
+    borderRadius: "10px"
   }
-}
+};
