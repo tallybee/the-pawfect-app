62822763f51d43f3bbf966c5422215a92ef5c7ed
rafaelrolivares <rafaelrolivares@gmail.com>
2019-05-17 13:20:50 +0200

    including hints for G1

--- src/reducers/breeds.js
+++ src/reducers/breeds.js
@@ -2,7 +2,8 @@ const initialState = {
   selectedDogBreed: '', 
   correctDogBreed: '',
   roundsPlayed: 0,
-  score: 0
+  score: 0,
+  previousBreeds: []
 };
 
 export default function reducer(state = initialState, action) {
@@ -19,14 +20,16 @@ export default function reducer(state = initialState, action) {
         ...state,
         selectedDogBreed: action.payload,
         score: state.score + 1,
-        roundsPlayed: state.roundsPlayed +1
+        roundsPlayed: state.roundsPlayed +1,
+        previousBreeds: [...state.previousBreeds, action.payload]
       };
     }
       case "WRONG_GUESS": {
         return {
           ...state,
           selectedDogBreed: action.payload,
-          roundsPlayed: state.roundsPlayed +1
+          roundsPlayed: state.roundsPlayed +1,
+          previousBreeds: [...state.previousBreeds, action.payload]
         }
       }
       case "START_NEW_GAME": {
