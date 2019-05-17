a94fc5b9baf2996eaecf0c7880a2a77b8dfa7673
limadebarros <limadebarros84@gmail.com>
2019-05-17 16:00:00 +0200

    game change

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,9 +16,9 @@ export default class Home extends Component {
         <p>We made two games for you, so you don’t pug-get any breeds! In <strong>“GUESS MY BREED”</strong>, look at a pic of doggo and choose which one it is. In <strong>“GUESS MY FACE”</strong>, you’ll have to figure out which of the pics belongs to the breed! And to finish off..</p>
         <p>Where do dogs go after their tails fall off?
         The re-tail store.</p>
-        <Link to={"/gameOne"} className='game-link'>GAME 1:</Link> &nbsp; You will see a pawcture of a doggo, and you will have to choose the right answer.
+        <Link to={"/gameOne"} className='game-link'>GUESS MY BREED:</Link> &nbsp; You will see a pawcture of a doggo, and you will have to choose the right answer.
         <br/>
-        <Link to={"/gameTwo"} className='game-link'>GAME 2:</Link> &nbsp; You will see 3 different doggos, and you will have to click on the right pawcture.
+        <Link to={"/gameTwo"} className='game-link'>GUESS MY FACE:</Link> &nbsp; You will see 3 different doggos, and you will have to click on the right pawcture.
       </div>
     )
   }
