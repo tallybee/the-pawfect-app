65853473e18a2f9211630dd8c528f60afdfa0a63
limadebarros <limadebarros84@gmail.com>
2019-05-17 14:58:51 +0200

    Home edited

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,8 @@ export default class Home extends Component {
     return (
       <div className='Home-app'>
         <h1>Welcome</h1>
-        <p>The Pawfect App is a game to test your knowledge about Doggos Breed. And before you take this pawesome chanllenge, you can always educate your self about it by going to <strong>DOGS LIST</strong>, there are 87 different breeds. But you think that you are a Doggo expect skipt it and show us what you got.</p>
-        <p>There is two games for you to Play.</p>
+        <p>The Pawfect App is a game to test your knowledge about Doggos Breed. And before you take this pawesome chanllenge, you can always educate your self about it by going to <strong>DOGS LIST</strong>, there are 87 different breeds. But you think that you are a Doggo expect skip it and show us what you got.</p>
+        <p>There is two games for you to Play. In each game you have 10 turns. To win you need to guess all 10 correctly</p>
         <Link to={"/gameOne"} className='game-link'>GAME 1:</Link> &nbsp; You will see a pawcture of a doggo, and you will have to choose the right answer.
         <br/>
         <Link to={"/gameTwo"} className='game-link'>GAME 2:</Link> &nbsp; You will see 3 different doggos, and you will have to click on the right pawcture.
